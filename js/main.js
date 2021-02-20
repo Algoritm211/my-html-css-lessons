16c023251782b1d626cfe87860e90d8bd3dd982b
alexey_horbunov <algoritm211@gmail.com>
2021-02-20 18:00:59 +0200

    Add modal window

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 
 window.addEventListener("DOMContentLoaded", () => {
+
+  // work with tabs
   const tabs = document.querySelectorAll(".tabs__item");
 
   tabs.forEach((tab) => {
@@ -16,6 +18,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // work with photo carousel
   $(".carousel").slick({
     autoPlay: true,
     autoPlaySpeed: 5000,
@@ -24,4 +27,65 @@ window.addEventListener("DOMContentLoaded", () => {
     nextArrow:
       `<div class="carousel__arrow carousel__next"><i class="fas fa-chevron-right"></i></div>`
   })
+
+  // work with video
+  const videos = document.querySelectorAll('#video')
+
+
+  videos.forEach((video) => {
+
+    const playBtn = video.nextSibling
+
+    playBtn.addEventListener('click', () => {
+      video.play()
+      video.setAttribute('controls', 'controls')
+      playBtn.style.display = 'none'
+    })
+
+
+    video.addEventListener('ended', function () {
+      this.src = this.src
+      playButton.style.display = 'block'
+      video.removeAttribute('controls')
+    })
+  })
+
+  // work with page block anchors
+  const anchors = document.querySelectorAll('.scroll-to')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      const blockId = anchor.getAttribute('href')
+
+      document.querySelector(blockId).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+
+  // work with popup window
+
+  function popupOpen() {
+    document.querySelector('.popup').classList.add('popup__active')
+    document.body.style.overflow = 'hidden'
+
+  }
+
+  function popupClose() {
+    document.querySelector('.popup').classList.remove('popup__active')
+    document.body.style.overflow = 'visible'
+  }
+
+  const buttonSignIn = document.querySelector('.useful__btn')
+  const buttonClosePopup = document.querySelector('.popup__close')
+
+  buttonSignIn.addEventListener('click', popupOpen)
+  buttonClosePopup.addEventListener('click', popupClose)
+
+
+
 })
+
